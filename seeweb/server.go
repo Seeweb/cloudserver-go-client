67d17fb48782f308bdd5ea3f4d584b087d34679a
José Antonio Reyes <trilogy.seaward0m@icloud.com>
2022-07-07 11:02:38 -0400

    add update, delete and list Server`s methods

--- seeweb/server.go
+++ seeweb/server.go
@@ -1,6 +1,9 @@
 package seeweb
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // ServerService handles the communication with server
 // related methods of the Seeweb API.
@@ -14,41 +17,113 @@ type PlanSize struct {
 
 // Server represents a server.
 type Server struct {
-	Name         string      `json:"name"`
-	Ipv4         string      `json:"ipv4"`
-	Ipv6         string      `json:"ipv6"`
-	Plan         string      `json:"plan"`
-	PlanSize     *PlanSize   `json:"plan_size"`
-	Location     string      `json:"location"`
-	Notes        string      `json:"notes"`
-	So           string      `json:"so"`
-	CreationDate time.Time   `json:"creation_date"`
-	DeletionDate interface{} `json:"deletion_date"`
-	ActiveFlag   bool        `json:"active_flag"`
-	Status       string      `json:"status"`
-	APIVersion   string      `json:"api_version"`
-	User         string      `json:"user"`
+	Name         string    `json:"name"`
+	Ipv4         string    `json:"ipv4"`
+	Ipv6         string    `json:"ipv6"`
+	Plan         string    `json:"plan"`
+	PlanSize     *PlanSize `json:"plan_size"`
+	Location     string    `json:"location"`
+	Notes        string    `json:"notes"`
+	So           string    `json:"so"`
+	CreationDate time.Time `json:"creation_date"`
+	DeletionDate time.Time `json:"deletion_date"`
+	ActiveFlag   bool      `json:"active_flag"`
+	Status       string    `json:"status"`
+	APIVersion   string    `json:"api_version"`
+	User         string    `json:"user"`
 }
 
-type SeewebCreateServerRequest struct {
+type Action struct {
+	ID           int       `json:"id"`
+	Status       string    `json:"status"`
+	User         string    `json:"user"`
+	CreatedAt    time.Time `json:"created_at"`
+	StartedAt    time.Time `json:"started_at"`
+	CompletedAt  time.Time `json:"completed_at"`
+	Resource     string    `json:"resource"`
+	ResourceType string    `json:"resource_type"`
+	Type         string    `json:"type"`
+	Progress     int       `json:"progress"`
+}
+
+type SeewebServerCreateRequest struct {
 	Plan     string `json:"plan"`
 	Location string `json:"location"`
 	Image    string `json:"image"`
 	Notes    string `json:"notes"`
 	SSHKey   string `json:"ssh_key"`
 }
-type SeewebCreateServerResponse struct {
+type SeewebServerCreateResponse struct {
 	Status   string  `json:"status"`
 	ActionID int     `json:"action_id"`
 	Server   *Server `json:"server"`
 }
 
+type SeewebServerListResponse struct {
+	Status string    `json:"status"`
+	Count  int       `json:"count"`
+	Server []*Server `json:"server"`
+}
+
+type SeewebServerDeleteResponse struct {
+	Status string  `json:"status"`
+	Action *Action `json:"action"`
+}
+
+type SeewebServerUpdateRequest struct {
+	Note  string `json:"note"`
+	Group string `json:"group"`
+}
+
+type SeewebServerUpdateResponse struct {
+	Status string `json:"status"`
+}
+
 // Create creates a new server.
-func (s *ServerService) Create(createServerRequest *SeewebCreateServerRequest) (*SeewebCreateServerResponse, *Response, error) {
+func (s *ServerService) Create(createServerRequest *SeewebServerCreateRequest) (*SeewebServerCreateResponse, *Response, error) {
 	u := "/servers"
-	v := new(SeewebCreateServerResponse)
+	v := new(SeewebServerCreateResponse)
 
 	resp, err := s.client.newRequestDo("POST", u, &createServerRequest, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// List lists all existing servers.
+func (s *ServerService) List() (*SeewebServerListResponse, *Response, error) {
+	u := "/servers"
+	v := new(SeewebServerListResponse)
+
+	resp, err := s.client.newRequestDo("GET", u, nil, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// Delete removes an existing server.
+func (s *ServerService) Delete(name string) (*SeewebServerDeleteResponse, *Response, error) {
+	u := fmt.Sprintf("/servers/%s", name)
+	v := new(SeewebServerDeleteResponse)
+
+	resp, err := s.client.newRequestDo("DELETE", u, nil, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// Update updates an existing server.
+func (s *ServerService) Update(name string, updateServerRequest *SeewebServerUpdateRequest) (*SeewebServerUpdateResponse, *Response, error) {
+	u := fmt.Sprintf("/servers/%s", name)
+	v := new(SeewebServerUpdateResponse)
+
+	resp, err := s.client.newRequestDo("PUT", u, &updateServerRequest, &v)
 	if err != nil {
 		return nil, nil, err
 	}
--- seeweb/server_test.go
+++ seeweb/server_test.go
@@ -2,6 +2,7 @@ package seeweb
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
@@ -12,7 +13,7 @@ func TestServerCreate(t *testing.T) {
 	setup()
 	defer teardown()
 
-	input := &SeewebCreateServerRequest{
+	input := &SeewebServerCreateRequest{
 		Plan:     "ECS1",
 		Location: "it-fr2",
 		Image:    "centos-7",
@@ -22,7 +23,7 @@ func TestServerCreate(t *testing.T) {
 
 	mux.HandleFunc("/servers", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "POST")
-		v := new(SeewebCreateServerRequest)
+		v := new(SeewebServerCreateRequest)
 		json.NewDecoder(r.Body).Decode(v)
 		if !reflect.DeepEqual(v, input) {
 			t.Errorf("Request body = %+v, want %+v", v, input)
@@ -35,7 +36,7 @@ func TestServerCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	want := &SeewebCreateServerResponse{
+	want := &SeewebServerCreateResponse{
 		Status:   "ok",
 		ActionID: 38,
 		Server: &Server{
@@ -47,7 +48,7 @@ func TestServerCreate(t *testing.T) {
 			Notes:        "my first server",
 			So:           "centos-7",
 			CreationDate: time.Date(2019, time.April, 30, 15, 19, 48, 535586000, time.FixedZone("", 0)),
-			DeletionDate: nil,
+			DeletionDate: time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
 			ActiveFlag:   false,
 			Status:       "Booting",
 			APIVersion:   "v4",
@@ -58,6 +59,124 @@ func TestServerCreate(t *testing.T) {
 				Disk: "20",
 			},
 		},
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
+func TestServerList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc(fmt.Sprintf("/servers/"), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		w.Write([]byte(`{"status":"ok","count":1,"server":[{"name":"ec200016","ipv4":"192.168.1.43","ipv6":"fe80::1","plan":"ECS1","plan_size":{"core":"1","ram":"1024","disk":"20"},"location":"it-fr2","notes":"my first server","so":"centos-7","creation_date":"2019-04-18T13:48:12.025548+00:00","deletion_date":null,"active_flag":true,"status":"Booted","api_version":"v4","user":"admin","virttype":"KVM"}]}`))
+	})
+
+	resp, _, err := client.Server.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &SeewebServerListResponse{
+		Status: "ok",
+		Count:  1,
+		Server: []*Server{
+			{
+				Name: "ec200016",
+				Ipv4: "192.168.1.43",
+				Ipv6: "fe80::1",
+				Plan: "ECS1",
+				PlanSize: &PlanSize{
+					Core: "1",
+					RAM:  "1024",
+					Disk: "20",
+				},
+				Location:     "it-fr2",
+				Notes:        "my first server",
+				So:           "centos-7",
+				CreationDate: time.Date(2019, time.April, 18, 13, 48, 12, 25548000, time.FixedZone("", 0)),
+				DeletionDate: time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+				ActiveFlag:   true,
+				Status:       "Booted",
+				APIVersion:   "v4",
+				User:         "admin",
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
+func TestServerDelete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	serverName := "ec200016"
+	input := serverName
+
+	mux.HandleFunc(fmt.Sprintf("/servers/%s", serverName), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		w.Write([]byte(`{"status":"ok","action":{"id":39,"status":"in-progress","user":"admin","created_at":"2019-04-30T16:33:03.317800+00:00","started_at":"2019-04-30T16:33:03.317019+00:00","completed_at":null,"resource":"ec200016","resource_type":"ECS","type":"delete_server","progress":0}}`))
+	})
+
+	resp, _, err := client.Server.Delete(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &SeewebServerDeleteResponse{
+		Status: "ok",
+		Action: &Action{
+			ID:           39,
+			Status:       "in-progress",
+			User:         "admin",
+			CreatedAt:    time.Date(2019, time.April, 30, 16, 33, 3, 317800000, time.FixedZone("", 0)),
+			StartedAt:    time.Date(2019, time.April, 30, 16, 33, 3, 317019000, time.FixedZone("", 0)),
+			CompletedAt:  time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+			Resource:     "ec200016",
+			ResourceType: "ECS",
+			Type:         "delete_server",
+			Progress:     0,
+		},
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
+func TestServerUpdate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	serverName := "ec200016"
+	input := &SeewebServerUpdateRequest{
+		Note:  "update server name",
+		Group: "eg103464",
+	}
+
+	mux.HandleFunc(fmt.Sprintf("/servers/%s", serverName), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		v := new(SeewebServerUpdateRequest)
+		json.NewDecoder(r.Body).Decode(v)
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	resp, _, err := client.Server.Update(serverName, input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &SeewebServerUpdateResponse{
+		Status: "ok",
 	}
 
 	if !reflect.DeepEqual(resp, want) {
