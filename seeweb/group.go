99593cd52778a4f13b3768a1d4bac89ec9baae24
José Antonio Reyes <trilogy.seaward0m@icloud.com>
2022-07-13 22:23:03 -0400

    add Create and Delete methods for Group

--- seeweb/group.go
+++ seeweb/group.go
@@ -1,5 +1,7 @@
 package seeweb
 
+import "fmt"
+
 // GroupService handles the communication with group
 // related methods of the Seeweb API.
 type GroupService service
@@ -34,6 +36,32 @@ func (a *GroupService) List() (*SeewebGroupListResponse, *Response, error) {
 	v := new(SeewebGroupListResponse)
 
 	resp, err := a.client.newRequestDo("GET", u, nil, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// Create creates a new group.
+func (s *GroupService) Create(createGroupRequest *SeewebGroupCreateRequest) (*SeewebGroupCreateResponse, *Response, error) {
+	u := "/groups"
+	v := new(SeewebGroupCreateResponse)
+
+	resp, err := s.client.newRequestDo("POST", u, &createGroupRequest, &v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// Delete removes an existing group.
+func (s *GroupService) Delete(id string) (*SeewebGroupDeleteResponse, *Response, error) {
+	u := fmt.Sprintf("/groups/%s", id)
+	v := new(SeewebGroupDeleteResponse)
+
+	resp, err := s.client.newRequestDo("DELETE", u, nil, &v)
 	if err != nil {
 		return nil, nil, err
 	}
--- seeweb/group_test.go
+++ seeweb/group_test.go
@@ -1,12 +1,13 @@
 package seeweb
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"reflect"
 	"testing"
 )
 
-// List lists all existing groups
 func TestGroupList(t *testing.T) {
 	setup()
 	defer teardown()
@@ -31,6 +32,71 @@ func TestGroupList(t *testing.T) {
 				Enabled: true,
 			},
 		},
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
+func TestGroupCreate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	input := &SeewebGroupCreateRequest{
+		Notes:    "Test group 1",
+		Password: "secret",
+	}
+
+	mux.HandleFunc("/groups", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		v := new(SeewebGroupCreateRequest)
+		json.NewDecoder(r.Body).Decode(v)
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+		w.Write([]byte(`{"status":"ok","group":{"id":2,"name":"eg100001","notes":"Test group 1","enabled":true}}`))
+	})
+
+	resp, _, err := client.Group.Create(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &SeewebGroupCreateResponse{
+		Status: "ok",
+		Group: &Group{
+			ID:      2,
+			Name:    "eg100001",
+			Notes:   "Test group 1",
+			Enabled: true,
+		},
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
+func TestGroupDelete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	groupID := "2"
+	input := groupID
+
+	mux.HandleFunc(fmt.Sprintf("/groups/%s", groupID), func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	resp, _, err := client.Group.Delete(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := &SeewebGroupDeleteResponse{
+		Status: "ok",
 	}
 
 	if !reflect.DeepEqual(resp, want) {
